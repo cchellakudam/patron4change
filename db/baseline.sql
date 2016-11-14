dcbe2831daaa8622feee06dd7b940a42ff3b4708
Lingfan <lingfangao@hotmail.com>
2016-11-14 03:51:01 +0100

    added baseline sql script, update README for backend setup, installed dependencies for postgres and ORM for project

---
+++ db/baseline.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE patron4change;
+
+-- connect to database
+\connect patron4change
+
+-- table to track database schema changes
+CREATE TABLE schema_changes(
+	id SERIAL PRIMARY KEY NOT NULL,
+	major_release VARCHAR(2) NOT NULL,
+	minor_release VARCHAR(4) NOT NULL,
+	script_name VARCHAR(50) NOT NULL,
+	date_applied TIMESTAMP NOT NULL
+);
+
+
+-- new baseline version inserts should be added here with each new merge of the baseline script
+INSERT INTO schema_changes VALUES (DEFAULT, '01', '0000', 'init script', CURRENT_TIMESTAMP);
