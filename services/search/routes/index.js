afa12818ad346f8fe8e4eced799608bd325529d7
Peter Hoffmann <peter@p-h.im>
2016-11-21 18:55:28 +0100

    #10773 Add basic search API

--- services/search/routes/index.js
+++ services/search/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import profile from './profile';
+import search from './search';
 
 const router = express.Router();
 export default router;
 
 router.use('/profile', profile);
+router.use('/search', search);
 
 router.get('/', (req, res) => {
   res.sendStatus(204);
---
+++ services/search/routes/search.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import elastic from '../elastic';
+
+const router = express.Router();
+export default router;
+
+router.get('/:type', (req, res) => {
+  elastic.search({
+    index: '',
+    type: req.params.type,
+    q: req.query.q
+  })
+  .then((result) => {
+    res.status(200).send(result.hits.hits);
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  });
+});
