697bd63d6345550da0a2a65946915b039332b931
Peter Hoffmann <peter@p-h.im>
2016-11-21 18:13:50 +0100

    #10773 Add basic test cases for search

--- test/unit/search/index.spec.js
+++ test/unit/search/index.spec.js
@@ -1,12 +1,10 @@
 import baseSpec from './base.spec';
 import profileSpec from './profile.spec';
+import searchSpec from './search.spec';
 
 describe('Search Service', () => {
 
   describe('Base', baseSpec);
-
   describe('/profile', profileSpec);
-
-  it('should find an inserted profile');
-  it('should\'nt find a deleted profile');
+  describe('/search', searchSpec);
 });
