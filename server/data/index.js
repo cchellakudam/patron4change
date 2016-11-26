0c6eccd4c4349e9c5b2341308e79d90eff06e958
Peter Hoffmann <peter@p-h.im>
2016-11-26 15:24:25 +0100

    #10716 Move data layer selection to config files

--- server/data/index.js
+++ server/data/index.js
@@ -1,11 +1,3 @@
+import config from 'config';
 
-import mocklayer from './mockdatalayer';
-import dataAccessLayer from './dataAccessLayer';
-
-const env = process.env.NODE_ENV;
-
-if('unit' === env){
-	module.exports = mocklayer;
-}else{
-	module.exports = dataAccessLayer;
-}
+module.exports = require('./' + config.get('dataLayer'));
