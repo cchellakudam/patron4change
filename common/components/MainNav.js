a636d31b8db8896d522414d0dd8282497e62c566
Marcel Zeilinger <marcel.zeilinger@live.at>
2016-12-12 00:37:18 +0100

    #11344 single-page routing

--- common/components/MainNav.js
+++ common/components/MainNav.js
@@ -2,13 +2,20 @@ import React from 'react';
 import {AppBar} from 'react-toolbox/lib/app_bar';
 import {Navigation} from 'react-toolbox/lib/navigation';
 import {Link} from 'react-toolbox/lib/link';
+import { browserHistory } from 'react-router';
 
 export default class MainNav extends React.Component {
+
+  onNavigateToSearch(e) {
+    e.preventDefault();
+    browserHistory.push('/search');
+  }
+
   render() {
     return <AppBar title='patron4change'>
 			<Navigation type='horizontal'>
 				<span>Changemaker</span>
-				<Link href="/search">Search</Link>
+				<Link href="/search" onClick={this.onNavigateToSearch}>Search</Link>
 			</Navigation>
 		</AppBar>;
   }
